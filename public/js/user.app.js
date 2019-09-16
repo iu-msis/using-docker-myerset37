a23e53493e6419a368ff3433897c099f711f5612
myerset37 <myerset77@gmail.com>
2019-09-15 22:29:14 -0400

    added vue and fixed formatting

---
+++ public/js/user.app.js
@@ -0,0 +1,24 @@
+var randomApp = new Vue ({
+  el: '#userApp',
+  data: {
+    users: {
+      name:"",
+      dob:"",
+      nat:"",
+      age:"",
+      picture:"",
+      phone:"",
+      email:""
+    }
+  },
+  methods: {
+    fetchUser() {
+      fetch('https://randomuser.me/api/')
+      .then(response => response.json())
+      .then(json => {randomApp.users = json.results[0];})
+    }
+  },
+  created(){
+    this.fetchUser()
+  }
+});
